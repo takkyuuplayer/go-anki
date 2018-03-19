8c404b176e45b59e5938b5769f9425d55b859b92
takkyuuplayer <takkyuuplayer@gmail.com>
2018-03-19 01:22:02 -0400

    Add MerriamWebster dictionary

---
+++ mw/mw.go
@@ -0,0 +1,36 @@
+package mw
+
+import (
+	"encoding/xml"
+	"errors"
+	"fmt"
+)
+
+type MerriamWebster struct {
+	apiKey     string
+	dictionary string
+}
+
+func New(key, dictionary string) *MerriamWebster {
+	return &MerriamWebster{key, dictionary}
+}
+
+func (m *MerriamWebster) GetSearchUrl(word string) string {
+	return fmt.Sprintf(
+		"https://www.dictionaryapi.com/api/v1/references/%s/xml/%s?key=%s",
+		m.dictionary,
+		word,
+		m.apiKey,
+	)
+}
+
+func (c *MerriamWebster) AnkiCard(body, word string) (string, error) {
+	var el EntryList
+	xml.Unmarshal([]byte(body), &el)
+
+	if len(el.Entries) == 0 {
+		return "", errors.New("Not Found")
+	}
+
+	return el.AnkiCard(word), nil
+}
