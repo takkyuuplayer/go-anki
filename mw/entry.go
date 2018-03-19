3db9c262782ed266e30fdc42c1a879bb55cf90f4
takkyuuplayer <takkyuuplayer@gmail.com>
2018-03-18 22:04:42 -0400

    Add template to make Anki card

--- mw/entry.go
+++ mw/entry.go
@@ -1,6 +1,12 @@
 package mw
 
-import "encoding/xml"
+import (
+	"bytes"
+	"encoding/xml"
+	"log"
+	"strings"
+	"text/template"
+)
 
 type EntryList struct {
 	XMLName     xml.Name `xml:"entry_list"`
@@ -10,19 +16,64 @@ type EntryList struct {
 }
 
 type Entry struct {
-	XMLName         xml.Name   `xml:"entry"`
-	ID              string     `xml:"id,attr"`
-	Pronunciation   string     `xml:"pr"`
-	FunctionalLabel string     `xml:"fl"`
-	Definition      Definition `xml:"def"`
+	XMLName         xml.Name     `xml:"entry"`
+	ID              string       `xml:"id,attr"`
+	HeadWord        string       `xml:"hw"`
+	Inflection      []Inflection `xml:"in"`
+	Pronunciation   string       `xml:"pr"`
+	FunctionalLabel string       `xml:"fl"`
+	Definition      Definition   `xml:"def"`
+}
+
+type Inflection struct {
+	InnerXML string `xml:",innerxml"`
 }
 
 type Definition struct {
 	Gram            string           `xml:"gram"`
+	SenseNumber     []string         `xml:"sn"`
 	DefinitionTexts []DefinitionText `xml:"dt"`
 }
 
 type DefinitionText struct {
-	VerbalIllustration []string `xml:"vi"`
-	InnerXML           string   `xml:",innerxml"`
+	VerbalIllustrations []VerbalIllustration `xml:"vi"`
+	InnerXML            string               `xml:",innerxml"`
+}
+
+type VerbalIllustration struct {
+	Text string `xml:",innerxml"`
+}
+
+func (dt DefinitionText) Def() string {
+	from := strings.Index(dt.InnerXML, ":")
+	to := strings.Index(dt.InnerXML, "<")
+
+	if to == -1 {
+		return dt.InnerXML[from+1:]
+	}
+
+	return dt.InnerXML[from+1 : to]
+}
+
+func (e *Entry) AnkiCard() string {
+	tmpl := template.Must(template.ParseFiles("anki.tmpl.html"))
+	buf := bytes.NewBufferString("")
+
+	if err := tmpl.Execute(buf, e); err != nil {
+		log.Fatalf("execution failed: %s", err)
+	}
+
+	return strings.Replace(buf.String(), "\n", "", -1)
+}
+
+func (el *EntryList) AnkiCard(headWord string) string {
+	ret := ""
+
+	for _, entry := range el.Entries {
+		if entry.HeadWord == headWord {
+			ret += entry.AnkiCard()
+		}
+	}
+
+	return ret
 }
--- mw/entry_test.go
+++ mw/entry_test.go
@@ -2,7 +2,9 @@ package mw_test
 
 import (
 	"encoding/xml"
+	"fmt"
 	"io/ioutil"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -21,6 +23,7 @@ func TestEntryListToParseDefinition(t *testing.T) {
 	entry := ret.Entries[0]
 
 	assert.Equal(t, "test[1]", entry.ID)
+	assert.Equal(t, "test", entry.HeadWord)
 	assert.Equal(t, "ˈtɛst", entry.Pronunciation)
 	assert.Equal(t, "noun", entry.FunctionalLabel)
 
@@ -29,9 +32,17 @@ func TestEntryListToParseDefinition(t *testing.T) {
 
 	definitionText := entry.Definition.DefinitionTexts[0]
 
-	assert.Equal(t, 9, len(definitionText.VerbalIllustration))
-	assert.Equal(t, "She is studying for her math/spelling/history .", definitionText.VerbalIllustration[0])
+	assert.Equal(t, 9, len(definitionText.VerbalIllustrations))
+	assert.Equal(t,
+		"She is studying for her math/spelling/history <it>test</it>.",
+		string(definitionText.VerbalIllustrations[0].Text),
+	)
 	assert.NotNil(t, definitionText.InnerXML)
+
+	senseNumber := entry.Definition.SenseNumber
+
+	assert.Equal(t, 6, len(senseNumber))
+	assert.Equal(t, "1", senseNumber[0])
 }
 
 func TestEntryListToParseSuggestion(t *testing.T) {
@@ -44,4 +55,34 @@ func TestEntryListToParseSuggestion(t *testing.T) {
 	assert.Equal(t, 2, len(ret.Suggestions))
 	assert.Equal(t, "furniture", ret.Suggestions[0])
 	assert.Equal(t, "fornicator", ret.Suggestions[1])
+}
+
+func TestDefinitionText(t *testing.T) {
+	data, _ := ioutil.ReadFile("../testdata/merriam-webster/test.xml")
+
+	ret := mw.EntryList{}
+	xml.Unmarshal([]byte(data), &ret)
+	entry := ret.Entries[0]
+	definitionText := entry.Definition.DefinitionTexts[0]
+
+	assert.Equal(t, "a set of questions or problems that are designed to measure a person's knowledge, skills, or abilities ", definitionText.Def())
+}
+
+func TestEntryToAnkiCard(t *testing.T) {
+	data, _ := ioutil.ReadFile("../testdata/merriam-webster/test.xml")
+	ret := mw.EntryList{}
+	xml.Unmarshal([]byte(data), &ret)
+
+	assert.Equal(t, true, strings.Contains(ret.Entries[0].AnkiCard(), "<h3>noun</h3>"))
+}
+
+func TestEntryListToAnkiCard(t *testing.T) {
+	data, _ := ioutil.ReadFile("../testdata/merriam-webster/test.xml")
+	ret := mw.EntryList{}
+	xml.Unmarshal([]byte(data), &ret)
+
+	fmt.Println(ret.AnkiCard("test"))
+
+	assert.Equal(t, false, strings.Contains(ret.AnkiCard("test"), "<h3>noun</h3>"))
+	assert.Equal(t, false, strings.Contains(ret.AnkiCard("wrong"), "<h3>noun</h3>"))
 }
