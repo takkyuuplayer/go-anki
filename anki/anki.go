45324ebab4c09505ffd74cab6595afad1f5895d1
takkyuuplayer <takkyuuplayer@gmail.com>
2020-12-16 00:18:56 +0900

    Fix race condition

--- anki/anki.go
+++ anki/anki.go
@@ -5,9 +5,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
-	"log"
 	"strings"
-	"sync"
 
 	"github.com/takkyuuplayer/go-anki/dictionary"
 )
@@ -19,63 +17,72 @@ const errorLimit = 10
 
 // Run generates anki card tsv file
 func Run(dic dictionary.Dictionary, in io.Reader, out, outErr *csv.Writer) {
-	c := make(chan bool, concurrency)
-	wg := &sync.WaitGroup{}
 	scanner := bufio.NewScanner(in)
-	errCount := 0
+	unkownErrCount := 0
+	type wordResult struct {
+		out []string
+		err error
+	}
+	c := make(chan wordResult, concurrency)
+
 	for scanner.Scan() {
 		word := strings.Trim(scanner.Text(), " ")
 		if word == "" {
 			continue
 		}
-
-		if errorLimit <= errCount {
+		if errorLimit <= unkownErrCount {
 			break
 		}
-		wg.Add(1)
+
 		go func(word string) {
-			defer func() {
-				c <- true
-				wg.Done()
-			}()
-
-			body, err := dic.LookUp(word)
+			res, err := RunWord(dic, word)
 			if err == dictionary.ErrNotFound {
-				errCount -= 1
-				outErr.Write([]string{errorNotFound, word})
-				return
+				if unkownErrCount > 0 {
+					unkownErrCount -= 1
+				}
 			} else if err != nil {
-				errCount += 1
-				log.Println(word, ": ", err)
-				outErr.Write([]string{errorUnknown, fmt.Sprintf("%s: %s", word, err)})
-				return
+				unkownErrCount += 1
+			} else {
+				if unkownErrCount > 0 {
+					unkownErrCount -= 1
+				}
 			}
-			result, err := dic.Parse(word, body)
-			if err == dictionary.ErrNotFound {
-				errCount -= 1
-				outErr.Write([]string{errorNotFound, word})
-				return
-			} else if err != nil {
-				errCount += 1
-				log.Println(word, ": ", err)
-				outErr.Write([]string{errorUnknown, fmt.Sprintf("%s: %s", word, err)})
-				return
-			}
-			if len(result.Suggestions) > 0 {
-				errCount -= 1
-				outErr.Write([]string{errorNotFound, word})
-				return
-			} else if len(result.Entries) > 0 {
-				errCount -= 1
-				card := Card{SearchWord: word, Entries: result.Entries}
-				back, _ := card.Back()
-				out.Write([]string{card.Front(), back})
-			}
+			c <- wordResult{res, err}
 		}(word)
-		<-c
-	}
-	wg.Wait()
 
+		res := <-c
+		if res.err == nil {
+			out.Write(res.out)
+		} else {
+			outErr.Write(res.out)
+		}
+	}
+	if unkownErrCount >= errorLimit {
+		outErr.Write([]string{errorUnknown, "Stopped because of too many unknown errors"})
+	}
 	out.Flush()
 	outErr.Flush()
+}
+
+func RunWord(dic dictionary.Dictionary, word string) ([]string, error) {
+	body, err := dic.LookUp(word)
+	if err == dictionary.ErrNotFound {
+		return []string{errorNotFound, word}, err
+	} else if err != nil {
+		return []string{errorUnknown, fmt.Sprintf("%s: %s", word, err)}, err
+	}
+
+	result, err := dic.Parse(word, body)
+	if err == dictionary.ErrNotFound {
+		return []string{errorNotFound, word}, err
+	} else if err != nil {
+		return []string{errorUnknown, fmt.Sprintf("%s: %s", word, err)}, err
+	}
+	if len(result.Suggestions) > 0 {
+		return []string{errorNotFound, word}, err
+	}
+
+	card := Card{SearchWord: word, Entries: result.Entries}
+	back, _ := card.Back()
+	return []string{card.Front(), back}, nil
 }
